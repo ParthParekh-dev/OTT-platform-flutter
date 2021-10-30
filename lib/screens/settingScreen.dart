c5cd40b4677448fbaeaeb554238861847e9e16c7
ParthParekh-dev <81976919+ParthParekh-dev@users.noreply.github.com>
2021-10-31 00:44:59 +0530

    Setting page back press to homscreen

--- lib/screens/settingScreen.dart
+++ lib/screens/settingScreen.dart
@@ -52,154 +52,60 @@ class _SettingScreenState extends State<SettingScreen> {
     }
   }
 
+  backPressed() {
+    Get.offAll(HomeScreen());
+  }
+
   @override
   Widget build(BuildContext context) {
     checkDeroute();
 
-    return SafeArea(
-      child: Scaffold(
-        body: ListView(
-          children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.07,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                    child: IconButton(
-                      onPressed: () {
-                        Get.offAll(() => HomeScreen());
-                      },
-                      icon: Icon(
-                        Icons.arrow_back,
-                        color: Colors.white,
+    return WillPopScope(
+      onWillPop: () => backPressed(),
+      child: SafeArea(
+        child: Scaffold(
+          body: ListView(
+            children: [
+              Container(
+                height: MediaQuery.of(context).size.height * 0.07,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                      child: IconButton(
+                        onPressed: () {
+                          Get.offAll(() => HomeScreen());
+                        },
+                        icon: Icon(
+                          Icons.arrow_back,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                      ),
+                    ),
+                    Image.asset('assets/logo.png'),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 15.0, vertical: 5.0),
+                      child: Icon(
+                        Icons.settings,
+                        color: Colors.black,
                         size: 40,
                       ),
                     ),
-                  ),
-                  Image.asset('assets/logo.png'),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 15.0, vertical: 5.0),
-                    child: Icon(
-                      Icons.settings,
-                      color: Colors.black,
-                      size: 40,
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 8.0),
-                    child: Container(
-                      width: MediaQuery.of(context).size.width,
-                      height: 45,
-                      decoration: BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                              color: Colors.black26,
-                              offset: Offset(0, 4),
-                              blurRadius: 5.0),
-                        ],
-                        gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          stops: [0.0, 1.0],
-                          colors: [
-                            Color(0xFFED0205),
-                            // Color(0xFFFF8F45),
-                            Color(0xFFFE8F54),
-                          ],
-                        ),
-                        color: Colors.deepPurple.shade300,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                          ),
-                          backgroundColor:
-                              MaterialStateProperty.all(Colors.transparent),
-                          // elevation: MaterialStateProperty.all(3),
-                          shadowColor:
-                              MaterialStateProperty.all(Colors.transparent),
-                        ),
-                        onPressed: () async {
-                          settingController.generateHash();
-                        },
-                        child: Obx(
-                          () => (settingController.isLoading.value)
-                              ? (CircularProgressIndicator())
-                              : Text(
-                                  'Account Settings',
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    color: Colors.white,
-                                  ),
-                                ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Divider(
-                      thickness: 2,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'notifications'.tr,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'notification_setting'.tr,
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      Switch(
-                        onChanged: toggleSwitch,
-                        value: isSwitched,
-                        activeColor: Colors.blue,
-                        activeTrackColor: Colors.yellow,
-                        inactiveThumbColor: Colors.redAccent,
-                        inactiveTrackColor: Colors.orange,
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Divider(
-                      thickness: 2,
-                      color: Colors.white,
-                    ),
-                  ),
-                  Text(
-                    'language'.tr,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'current_language'.tr,
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      Container(
-                        width: MediaQuery.of(context).size.width * 0.4,
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width,
                         height: 45,
                         decoration: BoxDecoration(
                           boxShadow: [
@@ -235,103 +141,204 @@ class _SettingScreenState extends State<SettingScreen> {
                             shadowColor:
                                 MaterialStateProperty.all(Colors.transparent),
                           ),
-                          onPressed: () {
-                            Get.to(LanguageScreen());
+                          onPressed: () async {
+                            settingController.generateHash();
                           },
-                          child: Text(
-                            'Change',
-                            style: TextStyle(
-                              fontSize: 15,
-                              color: Colors.white,
-                            ),
+                          child: Obx(
+                            () => (settingController.isLoading.value)
+                                ? (CircularProgressIndicator())
+                                : Text(
+                                    'Account Settings',
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                    ),
+                                  ),
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Divider(
-                      thickness: 2,
-                      color: Colors.white,
                     ),
-                  ),
-                  Text(
-                    'faq_help'.tr,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Divider(
-                      thickness: 2,
-                      color: Colors.white,
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Divider(
+                        thickness: 2,
+                        color: Colors.white,
+                      ),
                     ),
-                  ),
-                  Text(
-                    'privacy_policy'.tr,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: Divider(
-                      thickness: 2,
-                      color: Colors.white,
+                    Text(
+                      'notifications'.tr,
+                      style: TextStyle(fontSize: 16),
                     ),
-                  ),
-                  Text(
-                    'contact_us'.tr,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    'contact_idragon_team'.tr,
-                    style: TextStyle(fontSize: 16),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'email_profile'.tr,
-                        style: TextStyle(fontSize: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'notification_setting'.tr,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Switch(
+                          onChanged: toggleSwitch,
+                          value: isSwitched,
+                          activeColor: Colors.blue,
+                          activeTrackColor: Colors.yellow,
+                          inactiveThumbColor: Colors.redAccent,
+                          inactiveTrackColor: Colors.orange,
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Divider(
+                        thickness: 2,
+                        color: Colors.white,
                       ),
-                      Text(
-                        'support@idragonpro.com',
-                        style: TextStyle(fontSize: 16),
-                      )
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'contact'.tr,
-                        style: TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                      'language'.tr,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'current_language'.tr,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          height: 45,
+                          decoration: BoxDecoration(
+                            boxShadow: [
+                              BoxShadow(
+                                  color: Colors.black26,
+                                  offset: Offset(0, 4),
+                                  blurRadius: 5.0),
+                            ],
+                            gradient: LinearGradient(
+                              begin: Alignment.topCenter,
+                              end: Alignment.bottomCenter,
+                              stops: [0.0, 1.0],
+                              colors: [
+                                Color(0xFFED0205),
+                                // Color(0xFFFF8F45),
+                                Color(0xFFFE8F54),
+                              ],
+                            ),
+                            color: Colors.deepPurple.shade300,
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: ElevatedButton(
+                            style: ButtonStyle(
+                              shape: MaterialStateProperty.all<
+                                  RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                              ),
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.transparent),
+                              // elevation: MaterialStateProperty.all(3),
+                              shadowColor:
+                                  MaterialStateProperty.all(Colors.transparent),
+                            ),
+                            onPressed: () {
+                              Get.to(LanguageScreen());
+                            },
+                            child: Text(
+                              'Change',
+                              style: TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Divider(
+                        thickness: 2,
+                        color: Colors.white,
                       ),
-                      Text(
-                        'Whatsapp 9987714307',
-                        style: TextStyle(fontSize: 16),
-                      )
-                    ],
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        'customer_support'.tr,
-                        style: TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                      'faq_help'.tr,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Divider(
+                        thickness: 2,
+                        color: Colors.white,
                       ),
-                      Text(
-                        '+91 9819325845',
-                        style: TextStyle(fontSize: 16),
-                      )
-                    ],
-                  ),
-                ],
+                    ),
+                    Text(
+                      'privacy_policy'.tr,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: Divider(
+                        thickness: 2,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      'contact_us'.tr,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'contact_idragon_team'.tr,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'email_profile'.tr,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Text(
+                          'support@idragonpro.com',
+                          style: TextStyle(fontSize: 16),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'contact'.tr,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Text(
+                          'Whatsapp 9987714307',
+                          style: TextStyle(fontSize: 16),
+                        )
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'customer_support'.tr,
+                          style: TextStyle(fontSize: 16),
+                        ),
+                        Text(
+                          '+91 9819325845',
+                          style: TextStyle(fontSize: 16),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
